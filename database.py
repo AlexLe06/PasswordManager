8c8089c5e4d92fb0eb095e37bc34dabbd0cbda70
Alex Le <alexle@Alexs-MacBook-Air.local>
2026-09-17 08:34:49 -0400

    Added User Class

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-from sqlalchemy import create_engine
+import datetime
+from sqlalchemy import Column, String, Integer, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -12,3 +13,14 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, engine=engine)
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = "User"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, unique=False, nullable=False)
+    date_created = Column(datetime, default=datetime.datetime.utcnow)
+
+
+    
